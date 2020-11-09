d53e8ceca76fd0ce1656ea3b130f212afad68de9
xxxzhou <mfjt55@163.com>
2020-11-09 15:18:52 +0800

    android上测试agora模块

---
+++ android/05_livetest/src/main/java/aoce/samples/livetest/MainActivity2.java
@@ -0,0 +1,13 @@
+package aoce.samples.livetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
